18a1a204336a9a3b56b081659d63ad20be996af7
zyesimm <140653206+zyesimm@users.noreply.github.com>
2026-03-19 15:26:13 +0300

    feat: implement pitch rotation using vertical axis input
    W/S keys rotate the aircraft up and down

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -1,6 +1,17 @@
+// FlightController.cs
+// CENG454 - HW1: Sky-High Prototype
+// Author: Zeynep Aslan | Student ID: 210444028
+
+
 using UnityEngine;
 
 public class FlightController : MonoBehaviour
 {
-    
+    public float pitchSpeed = 45f;
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        transform.Rotate(-vertical * pitchSpeed * Time.deltaTime, 0f, 0f);
+    }
 }
