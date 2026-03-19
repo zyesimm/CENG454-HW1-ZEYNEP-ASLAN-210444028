736c7b28093bfb8fb1d300922cb2be6697130b57
zyesimm <140653206+zyesimm@users.noreply.github.com>
2026-03-19 15:33:35 +0300

    feat: implement yaw rotation using horizontal axis input
    A/D keys rotate the aircraft left and right

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -8,10 +8,17 @@ using UnityEngine;
 public class FlightController : MonoBehaviour
 {
     public float pitchSpeed = 45f;
+    public float yawSpeed = 60f;
 
     void Update()
     {
         float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Pitch (W/S)
         transform.Rotate(-vertical * pitchSpeed * Time.deltaTime, 0f, 0f);
+
+        // Yaw (A/D)
+        transform.Rotate(0f, horizontal * yawSpeed * Time.deltaTime, 0f);
     }
 }
