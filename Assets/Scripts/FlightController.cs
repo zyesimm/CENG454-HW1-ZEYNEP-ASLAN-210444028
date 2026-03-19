c0932a02a6ca878f9605c489f27aa2ca555d89b6
zyesimm <140653206+zyesimm@users.noreply.github.com>
2026-03-19 17:48:12 +0300

    refactor: split flight logic into rotation and thrust handlers

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -28,41 +28,20 @@ public class FlightController : MonoBehaviour
 
     void Update()
     {
-        float pitchInput = 0f;
-        float yawInput = 0f;
-
-        float roll = 0f;
-        if (Input.GetKey(KeyCode.Q))
-            roll = 1f;
-        if (Input.GetKey(KeyCode.E))
-            roll = -1f;
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(Vector3.forward * forwardSpeed *Time.deltaTime);
-        }
-        
-        
-
-        if (Input.GetKey(KeyCode.UpArrow))
-            pitchInput = -1f;
-        else if (Input.GetKey(KeyCode.DownArrow))
-            pitchInput = 1f;
-
-        transform.Rotate(Vector3.right * pitchInput * pitchSpeed * Time.deltaTime);
-
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            yawInput = -1f;
-        else if (Input.GetKey(KeyCode.RightArrow))
-            yawInput = 1f;
-
-        transform.Rotate(Vector3.up * yawInput * yawSpeed * Time.deltaTime);
-
-
-    
-
-        // Roll (Q/E)
-        transform.Rotate(0f, 0f, roll * rollSpeed * Time.deltaTime);
+        HandleRotation();
+        HandleThrust();
     }
+
+    private void HandleRotation()
+    {
+
+    }
+
+    private void HandleThrust()
+    {
+
+    }
+
+
+
 }
