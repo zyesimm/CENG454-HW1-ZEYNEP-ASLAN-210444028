d1bc85e111bbe2a391282a4a6f094fec251b6217
zyesimm <140653206+zyesimm@users.noreply.github.com>
2026-03-19 15:40:13 +0300

    feat: implement roll rotation using Q/E keys
    Rotates aircraft around Z axis and completes basic 3-axis flight control ( pitch, yaw, roll)

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -10,15 +10,28 @@ public class FlightController : MonoBehaviour
     public float pitchSpeed = 45f;
     public float yawSpeed = 60f;
 
+    public float rollSpeed = 80f;
+
+
     void Update()
     {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
+
+        float roll = 0f;
+        if (Input.GetKey(KeyCode.Q))
+            roll = 1f;
+        if (Input.GetKey(KeyCode.E))
+            roll = -1f;
+
 
         // Pitch (W/S)
         transform.Rotate(-vertical * pitchSpeed * Time.deltaTime, 0f, 0f);
 
         // Yaw (A/D)
         transform.Rotate(0f, horizontal * yawSpeed * Time.deltaTime, 0f);
+
+        // Roll (Q/E)
+        transform.Rotate(0f, 0f, roll * rollSpeed * Time.deltaTime);
     }
 }
